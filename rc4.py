cc2adc62b8f22b051484a35991ca92cf7c11975a
Gabriel Estevam Narciso <20170074318@A309-20L.imd.local>
2019-09-24 18:10:31 -0300

    Comentando o codigo

--- rc4.py
+++ rc4.py
@@ -1,5 +1,6 @@
 import binascii
 
+# Criação dos arrays para as permutações
 def array_S_T(key):
     S = []
     T = []
@@ -8,12 +9,14 @@ def array_S_T(key):
         T.append(ord(key[indice % len(key)]))
     return(S, T)
 
+# Mudança de posição dos elementos
 def change_position(array, index_i, element_i, index_j, element_j):
     array[index_i] = element_j
     array[index_j] = element_i
 
     return array
 
+# Algoritmo da permutação inicial do RC4
 def initial_permutation(array_S, array_T): 
     j = 0
     for i in range(256):
@@ -21,6 +24,7 @@ def initial_permutation(array_S, array_T):
         array_S = change_position(array_S, i, array_S[i], j, array_S[j])
     return array_S
 
+# Gerar um array da segunda permutação do RC4, para gerar os elementos
 def get_random_element(permutate_array):
     i = 0
     j = 0
@@ -30,6 +34,7 @@ def get_random_element(permutate_array):
         permutate_array[i], permutate_array[j] = permutate_array[j], permutate_array[i]
         yield permutate_array[(permutate_array[i] + permutate_array[j]) % 256]
 
+# Algoritmo para rodar o RC4 de acordo com o tamanho do texto e retorno do texto em hexadecimal
 def run_rc4(permutate_array, text):
     cipher_text = []
     random_char = get_random_element(permutate_array)
@@ -39,6 +44,7 @@ def run_rc4(permutate_array, text):
         cipher_text.append(chr(cipher_byte))
     return binascii.hexlify(''.join(cipher_text).encode())
 
+# Passos para decriptar a mensagem e retorno do texto claro
 def decrypt_message(text, key):
     S,T = array_S_T(key)
     S = initial_permutation(S, T)
@@ -48,6 +54,7 @@ def decrypt_message(text, key):
     text_de = binascii.unhexlify(natural_value[2:-1])
     return text_de.decode('utf-8')
 
+# Passos para encriptar a mensagem e retornar o o texto em hexadecimal
 def encrypt_message(text, key):
     S,T = array_S_T(key)
     S = initial_permutation(S, T)
